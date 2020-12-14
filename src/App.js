5116b51b9dc14a821c843b57a87c97bd5c2121fc
luisangelquinones <luisangelq3673@gmail.com>
2020-12-13 18:38:19 -0600

    Finish Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+import Image from "./cryptomonedas.png";
+import Form from "./components/Form";
+import Quote from "./components/Quote";
+import Spinner from "./components/Spinner";
+import Axios from "axios";
 
 function App() {
+  const [coin, saveCoin] = useState("");
+  const [crypto, saveCrypto] = useState("");
+  const [result, saveResult] = useState({});
+  const [loading, saveLoading] = useState(false);
+
+  useEffect(() => {
+    const quoteCrypto = async () => {
+      //we avoid the first execution
+      if (coin === "") return;
+
+      //consult api to get quote
+      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${crypto}&tsyms=${coin}`;
+
+      const result = await Axios.get(url);
+
+      //Show Spinner
+      saveLoading(true);
+
+      setTimeout(() => {
+        saveLoading(false);
+
+        saveResult(result.data.DISPLAY[crypto][coin]);
+      }, 2000);
+    };
+    quoteCrypto();
+  }, [coin, crypto]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={Image} alt="imageCrypto" />
+      </div>
+      <div>
+        <h1>Cotiza Criptomonedas</h1>
+
+        <Form saveCoin={saveCoin} saveCrypto={saveCrypto} />
+
+        {loading ? <Spinner /> : <Quote result={result} />}
+      </div>
     </div>
   );
 }
